8262cd387053dbeee4479194cc2cdef21a614151
PCordista <pcordista368@gmail.com>
2019-02-25 17:33:14 -0300

    JSON

---
+++ assets/js/rest.js
@@ -0,0 +1,12 @@
+{
+	"jobs": [
+		{
+			"id": "1",
+			"getIn": "2017/12",
+			"getOut": "Atualmente",
+			"business": "CreativeCode - Agencia Digital",
+			"job": "Desenvolvedor Front-End",
+			"activies": " HTML5/CSS3/JS/Jquery, integração com aplicações PHP, gestão de projetos, SEO, UX"
+		}
+	]
+}
